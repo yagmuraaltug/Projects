9c3916347e796912343c66ebbbc6ef60b12a5061
Yagmur <yagmuraaltug@gmail.com>
2021-02-11 18:14:53 +0300

    11.02.2020

---
+++ Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = new string[] { 
+            "Yagmur", "Unsal", "Ilgin"
+            };
+
+
+            List<string> names2 = new List<string> { "Yagmur", "Unsal", "Senem" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            names2.Add("Kamil");
+            Console.WriteLine(names2[3]);
+            Console.WriteLine(names2[0]);
+
+
+        }
+    }
+}
